45b3c472c5b7ed9e02b2a0a80d4c0b0e3b031d49
maxcore25 <mkorobov2002@mail.ru>
2022-02-27 09:05:32 +0300

    Header Item Component written and Header in process

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,21 @@
 import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UsersIcon,
+} from '@heroicons/react/outline';
 
 const Header = () => {
   return (
     <header className=''>
       <h1>This is header</h1>
+      <div>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+      </div>
       <Image
         className='object-contain'
         src='https://links.papareact.com/ua6'
