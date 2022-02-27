136b02e695a381e5904ae22dcd7c554ccee541ab
maxcore25 <mkorobov2002@mail.ru>
2022-02-27 08:09:38 +0300

    Image Component added

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,10 @@ const Header = () => {
     <header className=''>
       <h1>This is header</h1>
       <Image
+        className='object-contain'
         src='https://links.papareact.com/ua6'
-        width={100}
-        height={1000}
+        width={200}
+        height={100}
         alt='hello'
       />
     </header>
