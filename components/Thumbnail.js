ed40373e99b2f451b6a48b7506b29979ca771a79
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 19:34:34 +0300

    Thumbnail Details Added

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { ThumbUpIcon } from '@heroicons/react/outline';
 
 const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
@@ -15,6 +16,11 @@ const Thumbnail = ({ result }) => {
       <div className='p-2'>
         <p className='truncate max-w-md'>{result.overview}</p>
         <h2>{result.title || result.original_name}</h2>
+        <p>
+          {result.media_type && `${result.media_type} | `}
+          {result.release_date || result.first_air_date}
+          <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
+        </p>
       </div>
     </div>
   );
