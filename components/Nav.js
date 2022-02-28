c0150d27140c0e993bef4c28c00fa8f1cb9747ec
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 08:33:06 +0300

    More Endpoints added and Nav styling

--- components/Nav.js
+++ components/Nav.js
@@ -3,9 +3,15 @@ import requests from '../utils/requests';
 const Nav = () => {
   return (
     <nav>
-      {Object.entries(requests).map(([key, { title, url }]) => (
-        <h2 key={key}>{title}</h2>
-      ))}
+      <div className='flex'>
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className='cursor-pointer transition duration-100 transform hover:scale-125'>
+            {title}
+          </h2>
+        ))}
+      </div>
     </nav>
   );
 };
--- utils/requests.js
+++ utils/requests.js
@@ -13,4 +13,20 @@ export default {
     title: 'Action',
     url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
+  fetchComedyMovies: {
+    title: 'Comedy',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchHorrorMovies: {
+    title: 'Horror',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchRomanceMovies: {
+    title: 'Romance',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: 'Mystery',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
 };
