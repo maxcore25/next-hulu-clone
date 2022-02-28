76f308129210ee7f4694febc9da5b4a1c0f90373
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 13:55:53 +0300

    Horizontal Scrollbar hidden

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import requests from '../utils/requests';
 const Nav = () => {
   return (
     <nav>
-      <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll'>
+      <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
