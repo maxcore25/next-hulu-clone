c228a3ee2d8c218fa7461ccbcc7c73cebe172ba1
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 08:24:23 +0300

    Nav Output tested

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,13 @@
+import requests from '../utils/requests';
+
 const Nav = () => {
-  return <nav>Enter</nav>;
+  return (
+    <nav>
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2 key={key}>{title}</h2>
+      ))}
+    </nav>
+  );
 };
 
 export default Nav;
--- utils/requests.js
+++ utils/requests.js
@@ -5,4 +5,12 @@ export default {
     title: 'Trending',
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
+  fetchTopRated: {
+    title: 'Top Rated',
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: 'Action',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
 };
