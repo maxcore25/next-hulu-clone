5910f3c958f078034eddc3473fbbf0c4fe6d4fab
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 19:42:08 +0300

    Thumbnail Stylized

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
 const Thumbnail = ({ result }) => {
   return (
-    <div>
+    <div className='group p-2 cursor-pointer transition duration-200 transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
@@ -15,8 +15,10 @@ const Thumbnail = ({ result }) => {
       />
       <div className='p-2'>
         <p className='truncate max-w-md'>{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p>
+        <h2 className='mt-1 text-2xl text-white transition-all duration-100 group-hover:font-bold'>
+          {result.title || result.original_name}
+        </h2>
+        <p className='flex items-center opacity-0 group-hover:opacity-100'>
           {result.media_type && `${result.media_type} | `}
           {result.release_date || result.first_air_date}
           <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
