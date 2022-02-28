4d1df4af766fd9c33ee69c0e0f2b5a649c5b2eac
maxcore25 <mkorobov2002@mail.ru>
2022-02-28 19:27:46 +0300

    Thumbnail Cascade And Style Continued

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+
+const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+
 const Thumbnail = ({ result }) => {
-  return <div>Enter</div>;
+  return (
+    <div>
+      <Image
+        layout='responsive'
+        src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
+        height={1080}
+        width={1920}
+        alt={'Thumbnail'}
+      />
+      <div className='p-2'>
+        <p className='truncate max-w-md'>{result.overview}</p>
+        <h2>{result.title || result.original_name}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Thumbnail;
